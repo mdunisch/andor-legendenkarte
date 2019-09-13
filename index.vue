4dd3fe986258bb4ac6ebe6a52807442fe5ae68ad
Jesse Schlueter <jesse@helix360.de>
2019-09-13 17:09:10 +0200

    Added series name, if any

--- index.vue
+++ index.vue
@@ -4,7 +4,7 @@
       <div class="left_number">{{ number }}</div>
       <div class="right_number">{{ number }}</div>
       <div class="legend">
-        <div v-if="(!isNaN(number) && number != '')" class="series">Legende<br /><span>{{ number }}</span></div>
+        <div v-if="(!isNaN(number) && number != '')" class="series">{{ (series === '') ? 'Legende' : series }}<br /><span>{{ number }}</span></div>
         <div class="name">{{ name }}</div>
       </div>
       <div v-if="(cardData.type === 'letter' || cardData.type === 'end')" class="number"
@@ -44,6 +44,10 @@ export default {
     name: {
       type: String,
       required: true
+    },
+    series: {
+      type: String,
+      default: ''
     },
     number: {
       type: String,
